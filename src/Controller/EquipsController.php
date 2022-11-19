5929c8e1809f12a0783ef948c12179a5cb2a300c
Alejandro <alexportalescervero@gmail.com>
2022-11-19 17:04:36 +0100

    Commit Twig EquipsAlejandro

--- src/Controller/EquipsController.php
+++ src/Controller/EquipsController.php
@@ -1,11 +1,10 @@
 <?php
 
 namespace App\Controller;
-
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class EquipsController
+class EquipsController extends AbstractController
 {
     private $equips = array(
         array(
@@ -42,15 +41,13 @@ class EquipsController
                 return $equip["codi"] == $codi;
             }
         );
-        if (count($resultat) > 0) {
-            $resposta = "";
-            $resultat = array_shift($resultat); #torna el primer element
-            $resposta .= "<ul><li>" . $resultat["nom"] . "</li>" .
-                "<li>" . $resultat["cicle"] . "</li>" .
-                "<li>" . $resultat["curs"] . "</li>" .
-                "<li>" . implode(", ", $resultat["membres"]) . "</li>" . "</ul>";
-            return new Response("<html><body>$resposta</body></html>");
-        } else
-            return new Response("No s'ha trobat l'equip: " . $codi);
+        if (count($resultat) > 0)
+            return $this->render('dades_equip.html.twig', array(
+                'equip' => array_shift($resultat)
+            ));
+        else
+            return $this->render('dades_equip.html.twig', array(
+                'equip' => NULL
+            ));
     }
 }
